8df38790d2f018a278d919d7f47d1693334a5e81
YouSungWook <star.wookk@gmail.com>
2021-08-07 04:25:15 +0900

    1815

---
+++ baekjoon/1815.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <math.h>
+using namespace std;
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    deque<int> t;
+    deque<int> s;
+    for (int i = 0; i < n; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        t.push_back(tmp);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (t.back() == 1)
+        {
+            s.push_front(i);
+        }
+        else if (t.back() == 2)
+        {
+            int tmp = s.front();
+            s.pop_front();
+            s.push_front(i);
+            s.push_front(tmp);
+
+        }
+        else
+        {
+            s.push_back(i);
+        }
+        t.pop_back();
+    }
+    while(!s.empty()){
+        cout <<s.front()<<" ";
+        s.pop_front();
+
+    }
+
+    return 0;
+}
