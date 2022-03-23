febca11bf0fa839e9881fda7741f689bc3524647
YouSungWook <star.wookk@gmail.com>
2022-03-23 23:28:31 +0900

    123

--- .vscode/c++/baekjoon/16236.cpp
+++ .vscode/c++/baekjoon/16236.cpp
@@ -8,11 +8,33 @@
 #include <tuple>
 #include <set>
 using namespace std;
+int arr[21][21];
+int n;
+int sX,sY,sS;
+int cnt;
+void bfs(int x,int y){
+
+    cout  <<x<<y;
+
+}
 int main()
 {
     cin.tie(NULL);
     ios::sync_with_stdio(false);
-    cout <<"tt";
+    cin >>n;
+    sS=2;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cin>>arr[i][j];
+            if(arr[i][j] ==9){
+                sX =i;
+                sY =j;
+            }
+        }
+    }
+    bfs(sX,sY);
+
+    
 
     return 0;
 }
