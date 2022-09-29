c75b33b81488ec1ee8c8e7c994ea8dfc3891f577
starwook <star.wookk@gmail.com>
2022-09-30 01:12:21 +0900

    20220930

--- c++/baekjoon/3055.cpp
+++ c++/baekjoon/3055.cpp
@@ -9,117 +9,109 @@
 #include <set>
 using namespace std;
 char arr[51][51];
-int r,c;
-int xi[4] ={-1,1,0,0};
-int yi[4] ={0,0,-1,1};
+int varr[51][51];
+int r, c;
+int xi[4] = {-1, 1, 0, 0};
+int yi[4] = {0, 0, -1, 1};
 int sr;
 int sc;
-int dr;
-int dc;
-int wr,wc;
 int ans;
-int turn;
-queue<pair< int,pair<int,int> > > que;
-vector<pair<int,int> > tmp;
-vector<pair<int,int > > newTmp;
-void water(int x,int y){
-    
-    for(int i=0;i<4;i++){
-        int newX = x+xi[i];
-        int newY = y+yi[i];
-
-        if (newX >= 1 && newX <= r && newY >= 1 && newY<=c){
-            if(arr[newX][newY] != 'X' &&arr[newX][newY] !='S'){
-                arr[newX][newY] = '*';
-            }
-        }
-    }
-}
-void bfs(int num){
-    int tmp = turn;
-    while(!que.empty()){
-        if(tmp !=turn){
-            for (int i = 1; i <= r; i++)
-            {
-                for (int j = 1; j <= c; j++)
-                {
-                    if (arr[i][j] == '*')
-                    {
-                        newTmp.push_back(make_pair(i,j));
-                        
-                    }
-                }
-            }
-            for(int i=0;i<newTmp.size();i++){
-                water(newTmp[i].first,newTmp[i].second);
-            }
-            newTmp.clear();
-            cout<<turn<<" =turn\n";
-            for(int i=1;i<=r;i++){
-                for(int j=1;j<=c;j++){
-                    cout<<arr[i][j] <<" ";
-                }
-                cout<<"\n";
-            }
-            cout <<"\nㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n";
-            tmp =turn;
-        }
-        int x = que.front().second.first;
-        int y = que.front().second.second;
-        turn = que.front().first;
-        for (int i = 0; i < 4; i++)
-        {
-            int newX = x + xi[i];
-            int newY = y + yi[i];
-            if (newX >= 1 && newX <= r && newY >= 1 && newY <= c)
-            {
-                if(arr[newX][newY] =='.'){
-                    que.push(make_pair(turn+1,make_pair(newX,newY)));
-                }
-                if(arr[newX][newY] =='D'){
-                    cout<<"굴도착!\n";
-                    if(ans ==0){
-                        ans = turn;
-                    }
-                    else{
-                        ans = min(ans,turn);
-                    }
-                }
-            }
-        }
-        que.pop();
-        
-    }
-    
-}
+int endR,endC;
+char turn;
+struct where
+{
+    int x;
+    int y;
+    char z;
+};
+queue<where> que;
 int main()
 {
-    cin >>r>>c;
-    for(int i=1;i<=r;i++){
-        for(int j=1;j<=c;j++){
-            cin >>arr[i][j];
-            if(arr[i][j] =='D'){
-                dr = i;
-                dc =j;
+    cin >> r >> c;
+    where start;
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            cin >> arr[i][j];
+            if (arr[i][j] == '*')
+            {
+                where water;
+                water.x = i;
+                water.y = j;
+                water.z = '*';
+                que.push(water);
             }
-            if(arr[i][j] =='S'){
-                sr =i;
-                sc =j;
+            if (arr[i][j] == 'S')
+            {
+                
+                start.x = i;
+                start.y = j;
+                start.z = 'S';
+                varr[i][j] =1;
+            }
+            if(arr[i][j] =='D'){
+                endR = i;
+                endC = j;
 
             }
         }
     }
-    que.push(make_pair(0,make_pair(sr,sc)));
-    bfs(0);
-    if(ans ==0){
-        cout<<"KAKTUS";
+    que.push(start);
+    while (!que.empty())
+    {
+
+        where target = que.front();
+        que.pop();
+        //cout << x << "/" << y << "/" << que.front().z << "\n";
+        //print();
+        if (target.x== endR &&target.y ==endC )
+        {
+            break;
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            int newX = target.x + xi[i];
+            int newY = target.y + yi[i];
+            if (newX >= 1 && newX <= r && newY >= 1 && newY <= c)
+            {
+                if (target.z == '*')
+                {
+                    if (arr[newX][newY] == '.' || arr[newX][newY] == 'S')
+                    {
+                        arr[newX][newY] = '*';
+                        where water;
+                        water.x = newX;
+                        water.y = newY;
+                        water.z = '*';
+                        que.push(water);
+                    }
+                }
+                else
+                {
+                    if (arr[newX][newY] == '.' || arr[newX][newY] == 'D')
+                    {
+                        if (varr[newX][newY] == 0)
+                        {
+                            varr[newX][newY] = varr[target.x][target.y] + 1;
+                            where water;
+                            water.x = newX;
+                            water.y = newY;
+                            water.z = 'S';
+                            que.push(water);
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+    if(varr[endR][endC] ==0){
+        cout <<"KAKTUS\n";
     }
     else{
-        cout << ans;
+        cout<< varr[endR][endC]-1<<"\n";
     }
-    
 
-
-    
-  return 0;
+    return 0;
 }
