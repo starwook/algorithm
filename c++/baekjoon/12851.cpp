6cd7c5867134556e2455c96e47d4f3260d9ea764
starwook <star.wookk@gmail.com>
2022-10-20 21:10:40 +0900

    20221020

--- c++/baekjoon/12851.cpp
+++ c++/baekjoon/12851.cpp
@@ -6,82 +6,107 @@
 #include <math.h>
 #include <cstring>
 #include <tuple>
+#include <map>
 using namespace std;
 int n, k;
 int visited[100001];
-int how[100001];
+int dist[100001];
+int arr[100001];
+int minNum = -1;
+int howMuch;
+map<int, int> m;
+void check(int x)
+{
+}
+void bfs(int a)
+{
+
+    visited[a] = 1;
+    queue<int> que;
+    que.push(a);
+    while (!que.empty())
+    {
+        int x = que.front();
+        que.pop();
+        
+        visited[x] = 1;
+        if (x == k)
+        {
+            if (minNum == -1)
+            {
+                minNum = dist[x];
+                howMuch++;
+            }
+            else if (minNum == dist[x])
+            {
+
+                howMuch++;
+            }
+        }
+        int nx = x + 1;
+        if (nx == 8 || nx == 9)
+        {
+            cout <<nx <<":nx,"<< x << ":x," << dist[nx] << "\n";
+        }
+        if (nx < 50 && !visited[nx])
+        {
+            que.push(nx);
+            if(!dist[nx]){
+                dist[nx] = dist[x] + 1;
+            }
+            else{
+                dist[nx] = min(dist[nx] ,dist[x]+1);
+            }
+        }
+        nx = x - 1;
+        if (nx == 8 || nx == 9)
+        {
+            cout << nx << ":nx," << x << ":x," << dist[nx] << "\n";
+        }
+        if (nx >= 0 && !visited[nx])
+        {
+            que.push(nx);
+            if(!dist[nx]){
+                dist[nx] = dist[x] + 1;
+            }
+            else
+            {
+                dist[nx] = min(dist[nx], dist[x] + 1);
+            }
+        }
+        nx = x * 2;
+        if (nx == 8 || nx == 9)
+        {
+            cout << nx << ":nx," << x << ":x," << dist[nx] << "\n";
+        }
+        if (nx < 50&& !visited[nx])
+        {
+            que.push(nx);
+            if (!dist[nx])
+            {
+                dist[nx] = dist[x] + 1;
+            }
+            else
+            {
+                dist[nx] = min(dist[nx], dist[x] + 1);
+            }
+        }
+    }
+}
 int main()
 {
     cin.tie(NULL);
     ios::sync_with_stdio(false);
     cin >> n >> k;
-    queue<int> q;
-    q.push(n);
-    int cnt = 0;   //몇가지;
-    int total = 0; //몇번왔는지
-    int found = false;
-    int fin = false;
     if (n == k)
     {
-        total = 0;
-        cnt = 1;
+        cout << "0\n1";
     }
     else
     {
-        visited[n] = 1;
-        while (!q.empty())
-        {
-            
-            int tmp = q.front();
-            q.pop();
-            visited[tmp]=1;
-            visited[k] = 0;
-            cout <<tmp<<" ";
-            if(tmp>40){
-                break;
-            }
-            if(total ==0 && tmp == k){
-                cnt++;
-                total = how[tmp];
-                continue;
-            }
-            if(total !=0 && total ==how[tmp] && tmp ==k){
-                cnt++;
-                continue;
-            }
-            // int arr[3];
-            // arr[0] = tmp-1;
-            // arr[1] = tmp+1;
-            // arr[2] = tmp*2;
-            // for(int i=0;i<3;i++){
-            //     if(arr[i] >=0 && arr[i]<=100000){
-            //         if (!visited[arr[i]])
-            //         {
-            //             how[arr[i]] = how[tmp] + 1;
-            //             q.push(arr[i]);
-            //         }
-            //     }
-            // }
-            if(0<=tmp -1 && !visited[tmp-1]){
-                q.push(tmp-1);
-                how[tmp-1] =how[tmp]+1;
-            }
-            if (k>= tmp + 1 && !visited[tmp + 1])
-            {
-                q.push(tmp +1);
-                how[tmp + 1] = how[tmp] + 1;
-            }
-            if (k+1>=tmp*2 && !visited[tmp*2])
-            {
-                q.push(tmp*2);
-                how[tmp*2] = how[tmp] + 1;
-            }
-        }
+        bfs(n);
+        cout << minNum << "\n"
+             << howMuch;
     }
-    // for(int i=0;i<40;i++){
-    //     cout <<how[i] <<" ";
-    // }
-    cout << total << "\n"
-         << cnt;
     return 0;
 }
