e07002bf19df6fd26ca1d369d7f55d832ea631f2
starwook <star.wookk@gmail.com>
2022-11-18 18:31:13 +0900

    swea/1208.cpp

--- c++/swea/tempCodeRunnerFile.cpp
+++ c++/swea/tempCodeRunnerFile.cpp
@@ -1,49 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <math.h>
-#include <cstring>
-#include <tuple>
-#include <set>
-using namespace std;
-int building[1001];
-int ans;
-int howMuch(int where)
-{
 
-    int maxHeight = building[where];
-    for (int i = where - 2; i <= where + 2; i++)
-    {
-        if (i == where)
-        {
-            continue;
-        }
-        if (building[where] <= building[i])
-        {
-            return 0;
-        }
-        maxHeight = min(maxHeight, building[where] - building[i]);
-    }
-    return maxHeight;
+    diff = blockArr[maxIndex] - blockArr[minIndex];
+    return diff;
 }
 int main()
 {
     for(int i=1;i<=10;i++){
-        memset(building,0,sizeof(building));
-        int n;
-        cin>>n;
-        for(int j=1;j<=n;j++){
-            cin >>building[j];
-        }
-        cout<<"\n";
-        for(int j=3;j<=n-2;j++){
-            ans = ans+ howMuch(j);
-        }
-        cout<< "#"<<i<<" "<< ans;
-        ans =0;        
-    }
-    return 0;
-}
-
+        cin>>dump;
+        memset(blockArr,0,sizeof(
