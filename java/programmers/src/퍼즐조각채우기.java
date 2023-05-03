cbb2daa5b5a79e873345593ef48bcfaed4b2cd8d
starwook <star.wookk@gmail.com>
2023-05-03 21:01:13 +0900

    전력망을 둘로 나누기

---
+++ java/programmers/src/퍼즐조각채우기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public  class 퍼즐조각채우기 {
+    class Solution {
+        int top =0;
+        int right = 1;
+        int bottom =2;
+        int left =3;
+        String[][] visited = new String[50][50];
+        public int solution(int[][] game_board, int[][] table) {
+            int answer = -1;
+            return answer;
+        }
+    }
+}
