401b7e7f0230f497b5c8136381717bd243d85065
starwook <star.wookk@gmail.com>
2023-05-05 23:39:43 +0900

    자물쇠와 열쇠

---
+++ java/programmers/src/자물쇠와_열쇠.java
@@ -0,0 +1,8 @@
+public class 자물쇠와_열쇠 {
+    class Solution {
+        public boolean solution(int[][] key, int[][] lock) {
+            boolean answer = true;
+            return answer;
+        }
+    }
+}
