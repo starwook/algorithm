444b36689eb849cbf360e3ff50793c4a343b6e68
starwook <star.wookk@gmail.com>
2023-07-24 15:24:03 +0900

    1062 java

--- java/baekjoon/src/_1062.java
+++ java/baekjoon/src/_1062.java
@@ -1,60 +1,64 @@
 import java.util.*;
 public class _1062 {
-    public static int[] arr= new int[26];
+    public static boolean[] arr= new boolean[26];
     public static char[] startCheck = {'a','n','c','t','i'};
     public static int answer;
     public static int n,m;
-    public static int count =0;
     public static List<String> strings = new ArrayList<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
         m = scanner.nextInt();
-
-
-
-
-
         scanner.nextLine();
+        if(m<5) {
+            System.out.println("0");
+            return;
+        }
+        if(m==26){
+            System.out.println(n);
+            return;
+        }
         for(int i=0;i<n;i++) {
             String tmp = scanner.nextLine();
-            String newTmp = tmp.replaceAll("[antic]","");
+            String newTmp = tmp.replace("anta","");
+            newTmp = newTmp.replace("tica","");
             strings.add(newTmp);
         }
-        for(int i=0;i< startCheck.length;i++) arr[startCheck[i]-'a'] =1;
-        count =5;
-        for(int i=0;i<26;i++){
-            if(arr[i]==0) dfs(i);
-        }
+        arr['a' - 'a'] = true;
+        arr['c' - 'a'] = true;
+        arr['i' - 'a'] = true;
+        arr['n' - 'a'] = true;
+        arr['t' - 'a'] = true;
+        dfs(0,0);
         System.out.println(answer);
     }
-    public static void dfs(int index) {
-        arr[index] =1;//1.체크인
-        count++;
-        if(count==m){ // 2.다 체크 했다면(목적지라면)
+    public static void dfs(int index,int count) {
+        arr[index] =true;
+        if(count==m-5){ // 2.다 체크 했다면(목적지라면)
             calculateAnswer();
         }
         else{
-            for(int i=index+1;i<26;i++){ //3.순회
-                if(arr[i]==0){ //4.갈수 있는가
-                    dfs(i); //5.간다
+            for(int i=index;i<26;i++){ //3.순회
+                if(!arr[i]){ //4.갈수 있는가
+                    dfs(i,count+1); //5.간다
                 }
             }
         }
-        arr[index] =0;//6.체크아웃
-        count--;
+        arr[index] = false;
     }
     private static void calculateAnswer() {
         int tmpAnswer =0;
-//        for(int i=0;i<26;i++){
-//            if(arr[i]==1) System.out.print((char)(i+'a')+" ");
+//        for(int i=0;i<arr.length;i++ ) {
+//         if(arr[i]) System.out.print((char)(i+'a'));
 //        }
 //        System.out.println();
         for(int i=0;i<strings.size();i++){
             String tmp = strings.get(i);
             boolean isReadable = true;
             for(int j=0;j<tmp.length();j++){
-                if(arr[tmp.charAt(j)-'a']==0){
+                if(!arr[tmp.charAt(j)-'a']){
+
+
                     isReadable = false;
                     break;
                 }
