6ce8ef6693e22a20e6e8d4142f02d0da27413919
starwook <star.wookk@gmail.com>
2023-07-25 15:55:22 +0900

    1072 java

--- java/baekjoon/src/_1072.java
+++ java/baekjoon/src/_1072.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class _1072 {
     public static long totalGamePlayed, winPlayed;
     public static int originalNumber;
-    public static int answer = Integer.MAX_VALUE;
+    public static int answer = -1;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         totalGamePlayed = scanner.nextInt();
@@ -13,25 +13,24 @@ public class _1072 {
         }
         originalNumber = (int)(winPlayed*100/totalGamePlayed);
         findMinimumBinarySearch();
-        if(answer ==Integer.MAX_VALUE) System.out.println(-1);
-        else System.out.println(answer);
+        System.out.println(answer);
     }
-    public static int findMinimumBinarySearch(){
+    public static void findMinimumBinarySearch(){
         int start = 0;
         int end = 1000000000;
         float eY = 470000000+19230770;
 
-        int toBeNumber = originalNumber+1;
+
         while(start<=end){
             int mid = (start+end)/2;
             int number = (int)((mid+winPlayed)*100/(mid+totalGamePlayed));
-            if(number==toBeNumber) {
-                answer = Math.min(answer,mid);
+            if(number!=originalNumber) {
+                answer = mid;
                 end = mid-1;
+                continue;
             }
-            if(number<toBeNumber) start = mid+1;
-            if(number>toBeNumber) end = mid-1;
+            start=mid+1;
         }
-        return -1;
+
     }
 }
