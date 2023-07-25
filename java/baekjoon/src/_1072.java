77e85e772dd761ac563f84e7ad3701f657bee544
starwook <star.wookk@gmail.com>
2023-07-25 15:49:47 +0900

    1072 java

--- java/baekjoon/src/_1072.java
+++ java/baekjoon/src/_1072.java
@@ -1,28 +1,36 @@
 import java.util.*;
 public class _1072 {
-    public static float X,Y;
+    public static long totalGamePlayed, winPlayed;
     public static int originalNumber;
+    public static int answer = Integer.MAX_VALUE;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        X = scanner.nextInt();
-        Y = scanner.nextInt();scanner.nextLine();
-        if(X==Y){
+        totalGamePlayed = scanner.nextInt();
+        winPlayed = scanner.nextInt();scanner.nextLine();
+        if(totalGamePlayed == winPlayed){
             System.out.println(-1);
             return;
         }
-        originalNumber = (int)((Y/X)*100);
-        System.out.println(findMinimumBinarySearch());
+        originalNumber = (int)(winPlayed*100/totalGamePlayed);
+        findMinimumBinarySearch();
+        if(answer ==Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(answer);
     }
     public static int findMinimumBinarySearch(){
-        float start = Y;
-        float end = X;
-        int mid = (int)(start+end)/2;
+        int start = 0;
+        int end = 1000000000;
+        float eY = 470000000+19230770;
+
         int toBeNumber = originalNumber+1;
         while(start<=end){
-            int number = (int)((mid/X)*100);
-            if(number==toBeNumber) return (int)(mid-Y);
-            if(number<toBeNumber) end = mid-1;
-            if(number>toBeNumber) start = end+1;
+            int mid = (start+end)/2;
+            int number = (int)((mid+winPlayed)*100/(mid+totalGamePlayed));
+            if(number==toBeNumber) {
+                answer = Math.min(answer,mid);
+                end = mid-1;
+            }
+            if(number<toBeNumber) start = mid+1;
+            if(number>toBeNumber) end = mid-1;
         }
         return -1;
     }
