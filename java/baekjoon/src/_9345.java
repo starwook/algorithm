c911facac297406d413c6d6a1b9d43bab538051c
starwook <star.wookk@gmail.com>
2023-08-24 22:50:31 +0900

    9345 java

--- java/baekjoon/src/_9345.java
+++ java/baekjoon/src/_9345.java
@@ -36,10 +36,10 @@ public class _9345 {
                 int l = Integer.parseInt(st.nextToken())+1;
                 int r = Integer.parseInt(st.nextToken())+1;
                 if(choice==1){
-                    System.out.println(l+"!!"+r);
+
                     int min = findMinimum(1,treeSize,l,r,1);
                     int max = findMaximum(1,treeSize,l,r,1);
-                    System.out.println(min+"/"+max);
+
                     if(min==l&& max==r){
                         System.out.println("YES");
                     }
@@ -52,17 +52,12 @@ public class _9345 {
                     int right = minTree[treeSize+r-1];
                     minTree[treeSize+r-1] = left;
                     maxTree[treeSize+r-1] = left;
-                    minTree[treeSize+l-1] =right;
-                    minTree[treeSize+l-1] =right;
-                    update(1,treeSize,1,l,right);
-                    for(int i=1;i<treeSize*2;i++){
-                        System.out.print(maxTree[i]+" ");
-                    }
-                    System.out.println();
-                    update(1,treeSize, 1,r,left);
-                    for(int i=1;i<treeSize*2;i++){
-                        System.out.print(maxTree[i]+" ");
-                    }
+                    minTree[treeSize+l-1] = right;
+                    maxTree[treeSize+l-1] = right;
+                    updateMax(1,treeSize,1,l,right);
+                    updateMax(1,treeSize, 1,r,left);
+                    updateMin(1,treeSize,1,l,right);
+                    updateMin(1,treeSize,1,r,left);
                 }
             }
         }
@@ -76,27 +71,41 @@ public class _9345 {
         return Math.min(findMinimum(left,mid,targetLeft,targetRight,index*2),findMinimum(mid+1,right,targetLeft,targetRight,index*2+1));
     }
     public static int findMaximum(int left,int right,int targetLeft,int targetRight,int index){
-        if(right<targetLeft||left>targetRight) return 0;
+        if(right<targetLeft||left>targetRight) {
+            return 0;
+        }
         if(targetLeft<=left&&right<=targetRight){
             return maxTree[index];
         }
         int mid = (left+right)/2;
-        return Math.min(findMaximum(left,mid,targetLeft,targetRight,index*2),findMaximum(mid+1,right,targetLeft,targetRight,index*2+1));
+        return Math.max(findMaximum(left,mid,targetLeft,targetRight,index*2),findMaximum(mid+1,right,targetLeft,targetRight,index*2+1));
     }
-    public static void update(int left,int right,int index,int target,int afterTarget){
-        if(target>right||target<left) return;
+    public static int updateMax(int left, int right, int index, int target, int afterTarget){
+        if(target>right||target<left) return maxTree[index];
+//        maxTree[index] = Math.max(maxTree[index],afterTarget);
         if(left<=target&&target<=right){
             if(left==right){
-                minTree[index] =afterTarget;
-                maxTree[index] =afterTarget;
-                return;
+                return maxTree[index];
             }
-            minTree[index] = Math.min(minTree[index],afterTarget);
-            maxTree[index] = Math.max(maxTree[index],afterTarget);
             int mid =(left+right)/2;
-            update(left,mid,index*2,target,afterTarget);
-            update(mid+1,right,index*2+1,target,afterTarget);
+            int maxLeft = updateMax(left,mid,index*2,target,afterTarget);
+            int maxRight =updateMax(mid+1,right,index*2+1,target,afterTarget);
+            maxTree[index] = Math.max(maxLeft,maxRight);
         }
-
+        return maxTree[index];
+    }
+    public static int updateMin(int left, int right, int index, int target, int afterTarget){
+        if(target>right||target<left) return minTree[index];
+//        maxTree[index] = Math.max(maxTree[index],afterTarget);
+        if(left<=target&&target<=right){
+            if(left==right){
+                return minTree[index];
+            }
+            int mid =(left+right)/2;
+            int minLeft = updateMin(left,mid,index*2,target,afterTarget);
+            int minRight = updateMin(mid+1,right,index*2+1,target,afterTarget);
+            minTree[index] = Math.min(minLeft,minRight);
+        }
+        return minTree[index];
     }
 }
