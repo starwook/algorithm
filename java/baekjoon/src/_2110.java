01738c500360c29f269171d2ba9ef67e764d950d
starwook <star.wookk@gmail.com>
2024-10-19 00:15:59 +0900

    2110 java

--- java/baekjoon/src/_2110.java
+++ java/baekjoon/src/_2110.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class _2110 {
@@ -11,9 +12,35 @@ public class _2110 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        int minDistance =1;
+        int maxDistance = arr[N-1]-arr[0];
 
+        int answer =0;
+        while(minDistance<=maxDistance){
+            int midDistance = (minDistance+maxDistance)/2;
 
-
+            int index =0;
+            int cnt =1;
+            for(int i=1;i<N;i++){
+                if(arr[i]-arr[index]>=midDistance){
+                    cnt++;
+                    index =i;
+                }
+            }
+            if(cnt<C){
+                maxDistance = midDistance-1;
+            }
+            if(cnt>=C){//같을 경우도 플러스하여준다.
+                minDistance = midDistance+1;
+                answer = midDistance;
+            }
+        }
+        System.out.println(answer);
     }
 }
 
